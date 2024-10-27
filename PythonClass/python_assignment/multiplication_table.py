167fece06dde2e59271dabf5f277ce1eaf3c8aa1
Bamzy123 <stephenomotos@gmail.com>
2024-10-27 14:04:05 +0100

    assignment cont'd

---
+++ PythonClass/python_assignment/multiplication_table.py
@@ -0,0 +1,11 @@
+for a in range(10):
+  product_one = 1 * a
+  product_two = 2 * a
+  product_three = 3 * a
+  product_four = 4 * a
+  product_five = 5 * a
+  product_six = 6 * a
+  product_seven = 7 * a
+  product_eight = 8 * a
+  product_nine = 9 * a
+  print (f"1 * {a} = {product_one}  2 * {a} = {product_two}   3 * {a} = {product_three}  4 * {a} = {product_four}  5 * {a} = {product_five}  6 * {a} = {product_six}    7 * {a} = {product_seven}    8 * {a} = {product_eight}    9 * {a} = {product_nine}")
