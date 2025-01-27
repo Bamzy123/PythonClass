23ef7ecdb5d8ca1e6294cd1bb1847d6dd64b3c12
Bamzy123 <stephenomotos@gmail.com>
2025-01-27 11:16:40 +0100

    Dsa_Functions and Test

---
+++ PythonClass/dataStructureAlgorithm/DSA_Function/arrayFunction.py
@@ -0,0 +1,37 @@
+class arrayFunction:
+    def __init__(self, size):
+        self.size = 0
+        self.capacity = size
+        self.my_list = []
+
+    def isEmpty(self):
+        return len(self.my_list) == 0
+
+    def isFull(self) -> bool:
+        return len(self.my_list) == self.capacity
+
+    def append(self, element) -> bool:
+        self.my_list.append(element)
+        self.size += 1
+        return self.isFull()
+
+    def remove(self, element):
+        if element in self.my_list:
+            self.my_list.remove(element)
+            self.size -= 1
+        else:
+            raise TypeError("Element not found in the list")
+
+    def pop(self, index):
+        if index < 0 or index >= len(self.my_list):
+            return False
+        else:
+            self.size -= 1
+            return self.my_list.pop(index)
+
+    def insert(self, index, value) -> bool:
+        if index < 0 or index > len(self.my_list):
+            return False
+        else:
+            self.my_list.insert(index, value)
+            return True
---
+++ PythonClass/dataStructureAlgorithm/DSA_Test/arrayTest.py
@@ -0,0 +1,67 @@
+import unittest
+
+from DSA_Function.arrayFunction import arrayFunction
+
+
+class arrayTest(unittest.TestCase):
+
+    def setUp(self):
+        self.array_test = arrayFunction(5)
+
+    def test_isEmpty_initial(self):
+        self.assertTrue(self.array_test.isEmpty())
+
+    def test_isFull_initial(self):
+        self.assertFalse(self.array_test.isFull())
+
+    def test_append(self):
+        self.assertFalse(self.array_test.append(10))
+        self.assertFalse(self.array_test.append(20))
+        self.assertFalse(self.array_test.append(30))
+        self.assertFalse(self.array_test.append(40))
+        self.assertTrue(self.array_test.append(50))
+
+    def test_remove(self):
+        self.array_test.append(10)
+        self.array_test.append(20)
+        self.array_test.remove(10)
+        self.assertFalse(10 in self.array_test.my_list)
+        self.assertEqual(self.array_test.size, 1)
+
+        with self.assertRaises(TypeError):
+            self.array_test.remove(100)
+
+    def test_pop(self):
+        self.array_test.append(10)
+        self.array_test.append(20)
+        popped_value = self.array_test.pop(1)
+        self.assertEqual(popped_value, 20)
+        self.assertEqual(self.array_test.size, 1)
+
+        self.assertFalse(self.array_test.pop(10))
+
+    def test_insert(self):
+        self.array_test.append(10)
+        self.array_test.append(20)
+        self.assertTrue(self.array_test.insert(1, 15))
+        self.assertEqual(self.array_test.my_list[1], 15)
+
+        self.assertFalse(self.array_test.insert(10, 25))
+
+    def test_size_and_capacity(self):
+        self.array_test.append(10)
+        self.assertEqual(self.array_test.size, 1)
+        self.assertEqual(self.array_test.capacity, 5)
+
+    def test_combination(self):
+        self.array_test.append(10)
+        self.array_test.append(20)
+        self.array_test.append(30)
+        self.array_test.remove(20)
+        self.array_test.insert(1, 25)
+        self.array_test.pop(0)
+        self.assertEqual(self.array_test.my_list, [25, 30])
+
+
+if __name__ == '__main__':
+    unittest.main()
