acd7d5c43c136aa173a862b11e06d85b5e5e8b07
Bamzy123 <stephenomotos@gmail.com>
2025-02-16 13:38:38 +0100

    Bank

--- PythonClass/Account/Functions/bank.py
+++ PythonClass/Account/Functions/bank.py
@@ -1,24 +1,23 @@
-# from Functions.account import Account
-#
-#
-# class Banks:
-#     def __init__(self):
-#         self.accounts = {}
-#         self.next_account_number = 1000
-#
-#     def create_account(self, pin, name):
-#        if any(acct.name == name for acct in self.accounts.values()):
-#            raise ValueError("Account with name {} already exists")
-#        account_number = self.next_account_number
-#        self.accounts[account_number] = Account(account_number,pin, name)
-#        self.next_account_number += 1
-#        return account_number
-#
-#     def get_account(self, account_number):
-#         return self.accounts.get(account_number)
-#
-#     def deposit(self, acct1, amount):
-#         account = self.get_account(acct1)
-#         if not account:
-#             raise ValueError("Account not found")
-#         account.deposit(amount)
+from Functions.account import Account
+
+class Banks:
+    def __init__(self):
+        self.accounts = {}
+        self.next_account_number = 1000
+
+    def create_account(self, pin, name):
+       if any(acct.name == name for acct in self.accounts.values()):
+           raise ValueError("Account with name {} already exists")
+       account_number = self.next_account_number
+       self.accounts[account_number] = Account(account_number,pin, name)
+       self.next_account_number += 1
+       return account_number
+
+    def get_account(self, account_number):
+        return self.accounts.get(account_number)
+
+    def deposit(self, acct1, amount):
+        account = self.get_account(acct1)
+        if not account:
+            raise ValueError("Account not found")
+        account.deposit(amount)
