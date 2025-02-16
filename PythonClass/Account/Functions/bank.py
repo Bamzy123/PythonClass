94a1cd38c9656606bc917c76107bb80eff4f2472
Bamzy123 <stephenomotos@gmail.com>
2025-02-16 14:03:59 +0100

    bank update

--- PythonClass/Account/Functions/bank.py
+++ PythonClass/Account/Functions/bank.py
@@ -6,21 +6,20 @@ class Banks:
         self.next_account_number = 1000
 
     def create_account(self, pin, name):
-        if any(acct.name == name for acct in self.accounts.values()):
-            raise ValueError("Account with name {} already exists".format(name))
-        account_number = self.next_account_number
-        self.accounts[account_number] = Account(account_number, pin, name)
-        self.next_account_number += 1
-        return account_number
+       if any(acct.name == name for acct in self.accounts.values()):
+           raise ValueError("Account with name {} already exists")
+       account_number = self.next_account_number
+       self.accounts[account_number] = Account(account_number,pin, name)
+       self.next_account_number += 1
+       return account_number
 
     def get_account(self, account_number):
-        account = self.accounts.get(account_number)
+        return self.accounts.get(account_number)
+
+    def deposit(self, acct1, amount):
+        account = self.get_account(acct1)
         if not account:
             raise ValueError("Account not found")
-        return account
-
-    def deposit(self, account_number, amount):
-        account = self.get_account(account_number)
         account.deposit(amount)
 
     def withdraw(self, account_number, amount, pin):
