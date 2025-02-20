094de8dfe75badf92fc196d4b66fb5855a0e27d6
Bamzy123 <stephenomotos@gmail.com>
2025-02-20 11:34:59 +0100

    encode and decode

--- PythonClass/PythonProject/class_activity/function/human.py
+++ PythonClass/PythonProject/class_activity/function/human.py
@@ -1,32 +1,58 @@
-import string
+import bcrypt
 
-name = 'ayo'
-print (name)
-print(id(name))
-my_name = 'ayo'
-print(my_name)
-print(id(my_name))
+USER_DETAILS =  'user_details.txt'
 
-my_name = name
-print(id(my_name))
-name += " mide"
-print (name)
-print(id(name))
+def hashed_password(password):
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-print(name.split(" "))
+def save_to_file(username, password):
+    with open(USER_DETAILS, 'a') as file:
+        file.write(f'{username}, {hashed_password.decode('utf-8')}\n')
 
-print(name.strip())
+def register_user():
+    while True:
+        username = input("Enter your username: ")
+        if not username:
+            print("Enter a valid username")
+            continue
+        break
 
-names = ['esther', 'bimbola', 'stephen', 'moses']
-print(", ".join(names))
-letters = string.ascii_letters
-print(letters)
-print(list(letters))
-print(", ".join(letters))
-print("-".join(letters))
-print("_".join(letters))
-print(name)
+    while True:
+        password = input("Enter your password: ")
+        if not password:
+            print("Enter a valid password")
+            continue
+        break
+    save_to_file(username, password)
 
-username =  input("Enter your username: ")
-if username.isspace():
+def validate_user(username, password):
+    with open(USER_DETAILS, 'r') as file:
+        details = file.read()
+        for line in details.split('\n'):
+            stored_username, stored_password = line.split(':')
+            print(stored_password)
+            if username == stored_username:
+                return bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8'))
+
+def login_user():
     username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if validate_user(username, password):
+        print("Login successful")
+
+def main():
+    menu = """
+    1. register user
+    2. Login user
+    3. Exit
+    """
+
+    choice = input(menu)
+    match choice:
+
+        case '1':
+            register_user()
+        case '2':
+            login_user()
+        case '3':
+            print("Thank you for logging in again")
